89c97654d3626d7f4b67992841a2d6ad50c6f786
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2019-06-30 21:10:43 +0200

    work on visualization

--- Optim4Flux.jl
+++ Optim4Flux.jl
@@ -1,11 +1,15 @@
 module Optim4Flux
-using Optim, Flux, Zygote
+using LinearAlgebra, Optim, Flux, Zygote, Plots # TODO: replace for recipesbase
+import Base.copyto!
 
+gradlength(grads::Zygote.Grads) = sum(length(g[1]) for g in grads.grads)
+paramlength(params::Flux.Params) = sum(length, params.params)
+Base.zeros(grads::Zygote.Grads) = zeros(gradlength(grads))
+Base.zeros(pars::Flux.Params) = zeros(paramlength(pars))
 
 # Grads =============================================
-gradlength(grads) = sum(length(g[1]) for g in grads.grads)
 
-function flatten!(v, grads::Zygote.Grads)
+function copyto!(v::AbstractArray, grads::Zygote.Grads)
     @assert length(v) == gradlength(grads)
     s = 1
     for g in grads.grads
@@ -16,9 +20,7 @@ function flatten!(v, grads::Zygote.Grads)
     v
 end
 
-flatten(grads::Zygote.Grads) = flatten!(zeros(gradlength(grads)), grads::Zygote.Grads)
-
-function unflatten!(v, grads::Zygote.Grads)
+function copyto!(grads::Zygote.Grads, v::AbstractArray)
     s = 1
     for g in grads.grads
         l = length(g[2])
@@ -28,25 +30,20 @@ function unflatten!(v, grads::Zygote.Grads)
     grads
 end
 
-# @test unflatten!(flatten(grads), grads) == grads
 # Params =============================================
 
-paramlength(params) = sum(length, params.params)
-
-function flatten!(v, pars::Zygote.Params)
-    @assert length(v) == gradlength(pars)
+function copyto!(v::AbstractArray, pars::Flux.Params)
+    @assert length(v) == paramlength(pars)
     s = 1
-    for g in pars.pars
-        l = length(g[2])
-        v[s:s+l-1] .= vec(g[2])
+    for g in pars.params
+        l = length(g)
+        v[s:s+l-1] .= vec(g)
         s += l
     end
     v
 end
 
-flatten(pars::Zygote.Params) = flatten!(zeros(gradlength(pars)), pars::Zygote.Params)
-
-function unflatten!(v, pars::Zygote.Params)
+function copyto!(pars::Flux.Params, v::AbstractArray)
     s = 1
     for p in pars.params
         l = length(p)
@@ -56,20 +53,86 @@ function unflatten!(v, pars::Zygote.Params)
     pars
 end
 
-# @test unflatten!(flatten(pars), pars) == pars
 
-function Optim.OnceDifferentiable(loss, pars::Zygote.Params)
+function Optim.OnceDifferentiable(loss, pars::Flux.Params)
     grads = Zygote.gradient(loss, pars)
-    gradvec = flatten(grads)
+    gradvec = zeros(grads)
     gradfun = function (w)
-        unflatten!(w, pars)
+        copyto!(pars, w)
         grads = Zygote.gradient(loss, pars)
-        flatten!(gradvec, grads)
+        copyto!(gradvec, grads)
     end
     lossfun = function (w)
-        grads = unflatten!(v, grads)
+        copyto!(pars, w)
         loss()
     end
+    OnceDifferentiable(lossfun, gradfun)
+end
+
+@recipe function lossplot(loss::Function, pars::Flux.Params, l=0.1)
+    p       = zeros(pars)
+    copyto!(p,pars)
+    pcopy   = deepcopy(p)
+    n0      = norm(p)
+    dx,dy   = randn(length(p)),randn(length(p))
+    dx     *= n0*l/norm(dx)
+    dy     *= n0*l/norm(dy)
+    pertvec = LinRange(-1,1,20)
+    losses = map(Iterators.product(pertvec,pertvec)) do (lx,ly)
+        pi = p + lx*dx + ly*dy
+        copyto!(pars, pi)
+        loss()
+    end
+    copyto!(pars, pcopy)
+    seriestype := :contour
+    pertvec,pertvec,losses
+end
+
+end
+
+using Main.Optim4Flux, Optim, Zygote, Flux, Test
+##
+m = Chain(Dense(1,10,tanh),Dense(10,10,tanh) , Dense(10,1))
+x = randn(1,100)
+y = sin.(x)
+
+loss() = mean(abs2, m(x) .- y)
+@show loss()
+Zygote.refresh()
+pars = Flux.params(m)
+pars0 = deepcopy(pars)
+npars = Optim4Flux.paramlength(pars)
+# @test begin
+#     copyto!(pars, zeros(pars))
+#     all(all(iszero, p) for p in pars)
+# end
+# @test begin
+#     p = zeros(pars)
+#     copyto!(pars, 1:npars)
+#     copyto!(p, pars)
+#     p == 1:npars
+# end
+# grads = Zygote.gradient(loss, pars)
+# grads0 = deepcopy(grads)
+# @test begin
+#     copyto!(grads, zeros(grads))
+#     all(all(iszero,grads[k]) for k in keys(grads.grads))
+# end
+# @test begin
+#     p = zeros(grads)
+#     copyto!(grads, 1:npars)
+#     copyto!(p, grads)
+#     p == 1:npars
+# end
 
 
+# @test copyto!(copyto(grads), grads) == grads
+
+opt = ADAM(0.01)
+cb = ()-> @show loss()
+for i = 1:100
+    grads = Zygote.gradient(loss, pars)
+    Flux.Optimise.update!(opt, pars, grads)
+    plot(loss, pars,1) |> display
+    @show loss()
 end
